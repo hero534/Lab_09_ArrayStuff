4cb58837f673a478b9bbcf1728fad5b4b91e969a
hero534 <goldsmhr@mail.uc.edu>
2025-03-16 15:52:17 -0400

    Task 05 Complete

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Random rnd = new Random();
-        int[] dataPoints = new int[100];
-        int sum = 0;
-        double average = 0.00;
-            for (int i = 0; i < dataPoints.length; i++) {
-                dataPoints[i] = rnd.nextInt(100) + 1;
-            } for (int dataPoint : dataPoints) {
-                System.out.printf(dataPoint + " | ");
-            } for (int dataPoint : dataPoints) {
-            sum += dataPoint;
-            }
-            average = (double) sum / dataPoints.length;
-                System.out.println("\nThe sum of the random array dataPoints is: " + sum);
-                System.out.println("The average of the random array dataPoints is: " + average);
+        Scanner in = new Scanner(System.in);
+        int userInput = 0;
+        userInput = SafeInput.getRangedInt(in, "Enter an integer between 1 and 100: ", 1, 100);
+
+            System.out.println("You entered: " + userInput);
     }
 }
